2f65931a60a85cb689a197389068405e6e3991b3
PiedraL <joaquinlangone12@gmail.com>
2024-05-13 18:42:23 -0300

    Configuring

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,4 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 
+const app = express()
+const port = process.env.PORT ?? 3000
+
+app.use(cors())
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
 
