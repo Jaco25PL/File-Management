a857bf5d357dfe475ebb3167d4336efd0d4aed42
PiedraL <joaquinlangone12@gmail.com>
2024-05-15 22:34:52 -0300

    Working in endpoints (backend)

--- Backend/server.ts
+++ Backend/server.ts
@@ -10,3 +10,27 @@ app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 })
 
+// Required endpoints -> (post & get)
+
+app.post('/api/files', async (req, res) => {
+
+    // 1- Extract the files from the request
+    // 2- Validate that we've files
+    // 3- Validate mimetype
+    // 4- Transform the file to string
+    // 5- Transform the string to JSON
+    // 6- Save the JSON to db or memory
+    
+    // 7- Return 200 with the message and the JSON data
+        return res.status(200).json({ data: [] , message: 'The file was successfully uploaded'})
+})
+
+app.get('/api/users', async (req, res) => {
+
+    // 1. Extract the query param 'q' from the request
+    // 2. Validate that we've the query param
+    // 3. Filter the data from the db or memory
+    
+    // 4. Return 200 with the filtered data 
+        return res.status(200).json({ data: [] })
+})
