e33e6f22199ea4f2f56bf1007f83ff528cf3295c
PiedraL <joaquinlangone12@gmail.com>
2024-05-28 00:25:57 -0300

    Adding a debounce when searching in the uploaded file

--- Frontend/src/components/Search.tsx
+++ Frontend/src/components/Search.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { JSONFile } from "../types"
 import { searchData } from "../services/search"
+import { useDebounce } from "@uidotdev/usehooks"
 
 export function Search ({ initialData }: {initialData: JSONFile[]}) {
 
@@ -9,46 +10,52 @@ export function Search ({ initialData }: {initialData: JSONFile[]}) {
         const searchParams = new URLSearchParams(window.location.search) //get what is after the ? symbol
         return searchParams.get('q') ?? '' // if there is a q paramter get it
     })
-
     //const searchParams = new URLSearchParams(window.location.search).get('q')
     //create a new object to access to the search part (after ? symbol) of the URL and get the 'q' param
+
+    const debouncedSearch = useDebounce(search, 300) // debounce to avoid multiple api calls
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value)
     }
 
     useEffect(() => {
-        if ( search === '') {
+        if ( debouncedSearch === '') {
             window.history.pushState({}, '', window.location.pathname)
             return
         }
 
-        window.history.pushState({}, '', `?q=${search}`)
-    }, [search])
+        window.history.pushState({}, '', `?q=${debouncedSearch}`) // change the url 
+    }, [debouncedSearch])
 
     useEffect(() => {
-        if (!search) {
+        if (!debouncedSearch) {
             setData(initialData) // if no search set the entire file
             return
         }
 
-        searchData(search)
+        searchData(debouncedSearch)
             .then(resp => {
                 const newData = resp // We keep what we recover
                 if (newData) setData(newData) // and push it in the state
             })
-    } , [search, initialData] )
+    } , [debouncedSearch, initialData] )
 
     return (
         <div>
             <h2>Search</h2>
 
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <input 
                 onChange={handleSearch} 
                 type="search" 
                 name="search"
                 placeholder="Search..."
+                defaultValue={search}
                 />
             </form>
 
@@ -58,7 +65,7 @@ export function Search ({ initialData }: {initialData: JSONFile[]}) {
                         data.map(row => (
                             <li key={row.UserID}>
                                 <div>
-                                    <span><strong>Client</strong>:    {row.FirstName} - {row.Email}</span>
+                                    <span><strong>Client</strong>: {row.FirstName} - {row.Email}</span>
                                 </div>
                             </li>
                         ))
